b50ee483a10076ecc30b3b42150e3d41df80f4cf
aditihaiman <ahaiman00@stuy.edu>
2019-04-01 10:15:59 -0400

    first attempt

--- Radix.java
+++ Radix.java
@@ -5,16 +5,16 @@ public class Radix {
 
   public void radixsort(int[] data) {
     int max = getMax(data);
-    MyLinkedList[] buckets = new MyLinkedList[10];
+    MyLinkedList<Integer>[] buckets = new MyLinkedList[10];
     MyLinkedList<Integer> data1 = new MyLinkedList<Integer>();
     copy(data, data1);
-
     for(int x = max; x > 0; x /= 10) {
       int digits = 0;
       for(int y = 0; y < data.length; y++) {
         buckets[(data1.get(y)/(10^digits))%10].add(data1.get(y));
       }
       digits++;
+      extend(data1, buckets);
     }
 
 
@@ -31,6 +31,15 @@ public class Radix {
   public static void copy(int[] data, MyLinkedList<Integer> data1) {
     for(int x = 0; x < data.length; x++) {
       data1.add(data[x]);
+    }
+  }
+
+  public static void copy2()
+
+  public static void extend(MyLinkedList<Integer> data1, MyLinkedList<Integer>[] bucket) {
+    data1.clear();
+    for(int x = 0; x < bucket.length; x++) {
+      data1.extend(bucket[x]);
     }
   }
 
